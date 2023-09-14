090b2ba961af953a185f55f13ed0cc4f5dc380fd
Роман <110688570+MoskalchenkoRoman@users.noreply.github.com>
2023-09-14 20:10:10 +0300

    dz1

--- src/test/java/dz1/CalculatorTest.java
+++ src/test/java/dz1/CalculatorTest.java
@@ -1,4 +1,22 @@
+package dz1;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class CalculatorTest {
-  
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void calculatingDiscount() {
+        Calculator calculator = new Calculator();
+        double total = calculator.calculatingDiscount(500.5, 500);
+        assert total > 0 : "Сумма не может быть меньше 1";
+    }
+
 }
